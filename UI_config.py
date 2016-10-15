0b2c57dced96b94d718431274d456c3a20ef98c5
pdpro <prasanndutt@gmail.com>
2016-10-15 07:19:55 -0700

    Update UI_config.py
    UI must be custom according to chosen screen size

--- UI_config.py
+++ UI_config.py
@@ -1,1 +1,11 @@
 # Program to configure SPI based screen touch module and configure user-interface
+# There are several tutorials available for configuring TFT/LCD touch screen
+# Depending on the size of screen, we need to download the correct library and need to configure accordingly
+
+# After installation of library package, we need to do calibration test to fully calibrate the TFT screen
+
+# sudo apt-get install -y xinput-calibrator
+# select Menu -> Preferences -> Calibrate Touchscreen -> touch the four points one by one to finish calibration
+# www.waveshare.com/wiki/3.2inch_RPi_LCD_(B)
+# https://learn.adafruit.com/adafruit-pitft-28-inch-resistive-touchscreen-display-raspberry-pi/touchscreen-install-and-calibrate
+
