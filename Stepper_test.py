7ca7cfcf40b3dc9651018aabe4cc4cec9f8fab85
pdpro <prasanndutt@gmail.com>
2016-10-15 07:17:30 -0700

    Update Stepper_test.py
    GUI based stepper motor test

--- Stepper_test.py
+++ Stepper_test.py
@@ -1,1 +1,32 @@
-# Program to test stepper motor function properly
+# Program to test servo motor function properly used to control the two-way valve
+# http://razzpisampler.oreilly.com/ch05.html
+# Need to connect to HDMI display for this test
+
+from Tkinter import *
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+pwm = GPIO.PWM(18, 100)
+pwm.start(5)
+
+class App:
+
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        scale = Scale(frame, from_=0, to=180,
+              orient=HORIZONTAL, command=self.update)
+        scale.grid(row=0)
+
+
+    def update(self, angle):
+        duty = float(angle) / 10.0 + 2.5
+        pwm.ChangeDutyCycle(duty)
+
+root = Tk()
+root.wm_title('Servo Control')
+app = App(root)
+root.geometry("200x50+0+0")
+root.mainloop()
